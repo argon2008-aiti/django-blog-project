507f5f50314ec60333455bc91ad0fc17c6be0a46
argon2008-aiti <macpherson2008@yahoo.com>
2012-07-10 08:33:40 +0000

    Django-lab

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,37 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=60)
     body = models.TextField()
     created  = models.DateField()
     updated = models.DateField()
+    def __unicode__(self):
+        return self.title
+
 class Comment(models.Model):
     body = models.TextField()
     author = models.CharField(max_length=60)
     created = models.DateField()
     updated = models.DateField()
     post = models.ForeignKey(Post)        
-admin.site.register(Post)
-admin.site.register(Comment)
+    def __unicode__(self):
+        return self.body
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created','updated',)
+    search_fields = ('title','body',)
+    list_filter = ('created',)
+    inlines = [ CommentInline, ]
+                
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post','author','body'[:60],'created','updated',)
+    list_filter = ('created','author',)
+    
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
+
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+"""
+This code should be copied and pasted into your blog/views.py file before you begin working on it.
+"""
+
+from django.template import Context, loader, Template
+from django.http import HttpResponse
+
+from models import Post, Comment
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    print type(post_list)
+    print post_list
+    return HttpResponse(post_list)
+
+def post_detail(request, id, showComments):
+    post_matched = Post.objects.get(id=id)
+    post_details = post_matched.body
+    post_comment = Comment.objects.get(post=id)
+    response = HttpResponse() 
+    post_html = Template('<h2>{{post_matched}}</h2>')
+    c_1 = Context({'post_matched':post_matched})
+    response.write(post_html.render(c_1))    
+    response.write(post_details)
+
+    if showComments=='true':    
+        response.write('<h3>Comments</h3>')
+        response.write(post_comment)
+        return response
+    return response
+
+def post_search(request, anything):
+    search_result = Post.objects.filter(body__contains=anything)
+    t = Template('<h3>All posts containing "{{anything}}"</h3>')
+    c = Context({'anything':anything})
+    response = HttpResponse()
+    response.write(t.render(c))
+    for post in search_result:
+        response.write(post.body)
+        response.write('</p>')
+    return response
+
+def home(request):
+    print 'it works'
+    return HttpResponse('hello world. Ete zene?') 
